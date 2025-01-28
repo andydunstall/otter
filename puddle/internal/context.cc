b039dd336a045c2d7499c38da9d86944dba0e8cd
Andrew Dunstall <andydunstall@hotmail.co.uk>
2025-01-28 05:32:50 +0000

    context: add task context

--- puddle/internal/context.cc
+++ puddle/internal/context.cc
@@ -1,1 +1,12 @@
 #include "puddle/internal/context.h"
+
+namespace puddle {
+namespace internal {
+
+boost::context::fiber Context::Terminate() {
+  // TODO(andydunstall)
+  return {};
+}
+
+}  // namespace internal
+}  // namespace puddle
--- puddle/internal/context.h
+++ puddle/internal/context.h
@@ -2,6 +2,7 @@
 
 #include <chrono>
 
+#include "boost/context/fiber.hpp"
 #include "boost/intrusive/list.hpp"
 #include "boost/intrusive/set.hpp"
 #include "boost/intrusive_ptr.hpp"
@@ -23,6 +24,14 @@ class Context {
   friend void intrusive_ptr_add_ref(Context* c) noexcept;
   friend void intrusive_ptr_release(Context* c) noexcept;
 
+ protected:
+  static constexpr size_t kStackSize = 64 * 1024;
+
+  boost::context::fiber Terminate();
+
+  // Holds the tasks execution context when the task is not active.
+  boost::context::fiber context_;
+
  private:
   // Required to access intrusive member hooks.
   friend Scheduler;
@@ -41,6 +50,69 @@ inline void intrusive_ptr_add_ref(Context* c) noexcept {
 
 inline void intrusive_ptr_release(Context* c) noexcept {
   // TODO(andydunstall)
+}
+
+// TaskContext is a context to run the given function.
+template <typename Fn, typename... Arg>
+class TaskContext final : public Context {
+ public:
+  TaskContext(const boost::context::preallocated& palloc,
+              boost::context::fixedsize_stack salloc, Fn&& fn, Arg... arg);
+
+  // Allocates a task context and stack.
+  static boost::intrusive_ptr<Context> Spawn(Fn&& fn, Arg&&... arg);
+
+ private:
+  boost::context::fiber Run(boost::context::fiber&& c);
+
+  typename std::decay<Fn>::type fn_;
+
+  std::tuple<Arg...> arg_;
+};
+
+template <typename Fn, typename... Arg>
+TaskContext<Fn, Arg...>::TaskContext(const boost::context::preallocated& palloc,
+                                     boost::context::fixedsize_stack salloc,
+                                     Fn&& fn, Arg... arg)
+    : Context{}, fn_(std::forward<Fn>(fn)), arg_(std::forward<Arg>(arg)...) {
+  context_ = boost::context::fiber{
+      std::allocator_arg, palloc, salloc,
+      std::bind(&TaskContext::Run, this, std::placeholders::_1)};
+}
+
+template <typename Fn, typename... Arg>
+boost::intrusive_ptr<Context> TaskContext<Fn, Arg...>::Spawn(Fn&& fn,
+                                                             Arg&&... arg) {
+  // Allocate a stack for the context, then add the context structure at the
+  // start of the stack.
+  //
+  // Copied from boost fibers make_worker_context_with_properties.
+
+  typedef TaskContext<Fn, Arg...> context_t;
+
+  boost::context::fixedsize_stack salloc{Context::kStackSize};
+  auto sctx = salloc.allocate();
+
+  void* storage =
+      reinterpret_cast<void*>((reinterpret_cast<uintptr_t>(sctx.sp) -
+                               static_cast<uintptr_t>(sizeof(context_t))) &
+                              ~static_cast<uintptr_t>(0xff));
+  void* stack_bottom = reinterpret_cast<void*>(
+      reinterpret_cast<uintptr_t>(sctx.sp) - static_cast<uintptr_t>(sctx.size));
+  const std::size_t size = reinterpret_cast<uintptr_t>(storage) -
+                           reinterpret_cast<uintptr_t>(stack_bottom);
+  return boost::intrusive_ptr<Context>{new (storage) context_t{
+      boost::context::preallocated{storage, size, sctx}, salloc,
+      std::forward<Fn>(fn), std::forward<Arg>(arg)...}};
+}
+
+template <typename Fn, typename... Arg>
+boost::context::fiber TaskContext<Fn, Arg...>::Run(boost::context::fiber&& c) {
+  // Run the user function then terminate the context.
+  auto fn = std::move(fn_);
+  auto arg = std::move(arg_);
+  std::apply(std::move(fn), std::move(arg));
+  return Terminate();
 }
 
 }  // namespace internal
